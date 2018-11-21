9f9d141521d30ce8e0e7b2551d2a3094d1142c34
Luke Osborne <lukewosborne@gmail.com>
2018-11-21 18:46:08 -0500

    red

--- rust/tests/tests.rs
+++ rust/tests/tests.rs
@@ -5,3 +5,11 @@ use bowling_game_rust::Game;
 fn test_create_game() {
     let mut g = Game::new();
 }
+
+#[test]
+fn test_gutter_game() {
+    let mut g = Game::new();
+    for i in 0..20 {
+        g.roll(0);
+    }
+}

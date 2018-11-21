5fbdb2249e2041b0472060d0cb590bb68b57f45e
Luke Osborne <lukewosborne@gmail.com>
2018-11-21 18:59:40 -0500

    refactor.1

--- rust/tests/tests.rs
+++ rust/tests/tests.rs
@@ -4,8 +4,10 @@ use bowling_game_rust::Game;
 #[test]
 fn test_gutter_game() {
     let mut g = Game::new();
-    for i in 0..20 {
-        g.roll(0);
+    let mut n = 20;
+    let mut pins = 0;
+    for i in 0..n {
+        g.roll(pins);
     }
     assert_eq!(0, g.score());
 }

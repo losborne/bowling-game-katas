a8074380051c5828b8e0022c050360990cdf17ca
Luke Osborne <lukewosborne@gmail.com>
2018-11-21 19:13:02 -0500

    refactor

--- rust/tests/tests.rs
+++ rust/tests/tests.rs
@@ -1,22 +1,23 @@
 extern crate bowling_game_rust;
 use bowling_game_rust::Game;
 
+
 #[test]
 fn test_gutter_game() {
-    let mut g = Game::new();
-    let mut n = 20;
-    let mut pins = 0;
-    for i in 0..n {
-        g.roll(pins);
-    }
-    assert_eq!(0, g.score());
+    let mut game = Game::new();
+    roll_many(20, 0, &mut game);
+    assert_eq!(0, game.score());
 }
 
 #[test]
 fn test_all_ones() {
-    let mut g = Game::new();
-    for i in 0..20 {
-        g.roll(1);
+    let mut game = Game::new();
+    roll_many(20, 1, &mut game);
+    assert_eq!(20, game.score());
+}
+
+fn roll_many(n : u16, pins : u16, game : &mut Game) {
+    for i in 0..n {
+        game.roll(pins);
     }
-    assert_eq!(20, g.score());
 }
